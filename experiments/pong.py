930a15c425f33033f5eb5f446ce933ccb3cecbfe
DQ.YE <yedanqi@example.com>
2022-01-17 17:59:06 +0800

    Add arguments to pong script

--- experiments/pong.py
+++ experiments/pong.py
@@ -1,10 +1,18 @@
 """ Let's make DQN Agent learn pong """
 
-from stable_baselines3.common.cmd_util import make_atari_env
-from stable_baselines3.common.vec_env import VecFrameStack
-
+import argparse
 from agents import DQNAgent
 from network import MinhDQN
+
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+
+# Command line parser
+parser = argparse.ArgumentParser(description="Atari Pong parameters")
+parser.add_argument("--train", dest="is_train_mode", action="store_true")
+parser.add_argument("--eval", dest="is_train_mode", action="store_false")
+parser.add_argument("--load-model", '-l', dest="model_path", default=None, type=str)
+args = parser.parse_args()
 
 # Instantiate environment
 env = make_atari_env("PongNoFrameskip-v4")
@@ -21,5 +29,12 @@ n_actions = env.action_space.n
 model = MinhDQN(nch, n_actions)
 agent = DQNAgent(env, model)
 
-# Training loop for 100 episodes
-agent.train(render=True)
+# Load pretrained model if path is given
+if args.model_path:
+    agent.load(args.model_path)
+
+# Either train or evaluate the model
+if args.is_train_mode:
+    agent.train(render=True)
+else:
+    agent.eval_model()
