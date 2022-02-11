c507b2192d7d6c6c9cde81145185a97f630756af
NUS <nothingistrue>
2022-02-11 17:24:32 +0800

    Evaluate A2C model in stable baselines

--- baselines/pong.py
+++ baselines/pong.py
@@ -2,10 +2,10 @@ import argparse
 
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
-from stable_baselines3 import DQN
+from stable_baselines3 import DQN, A2C
 
 # Instantiate environment
-env = make_atari_env("PongNoFrameskip-v4")
+env = make_atari_env("PongNoFrameskip-v4", seed=0)
 # Frame-stacking with 4 frames
 env = VecFrameStack(env, n_stack=4)
 
@@ -16,13 +16,11 @@ parser.add_argument("--eval", dest="is_train_mode", action="store_false")
 parser.add_argument("--load-model", '-l', dest="model_path", default=None, type=str)
 args = parser.parse_args()
 
-
-
 # Load pretrained model if path is given
 if args.model_path:
-    model = DQN.load(args.model_path)
+    model = A2C.load(args.model_path)
 else:
-    model = DQN("CnnPolicy", env, verbose=1)
+    model = A2C("CnnPolicy", env, verbose=1)
 
 if args.is_train_mode:
     model.learn(total_timesteps=1000000, log_interval=10)
@@ -30,7 +28,7 @@ if args.is_train_mode:
 else:
     obs = env.reset()
     while True:
-        action, _states = model.predict(obs, deterministic=True)
+        action, _ = model.predict(obs, deterministic=True)
         obs, reward, done, info = env.step(action)
         env.render()
         if done:
